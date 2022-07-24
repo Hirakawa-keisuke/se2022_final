74cf2bf8d1b453752fa284b943492c8f4ab6ee9a
lesson1 <b2201980@photon.chitose.ac.jp>
2022-07-25 00:25:36 +0900

    リプライ用のクラスを作成した

--- linebot-main/src/main/java/com/example/linebot/replier/BookOn.java
+++ linebot-main/src/main/java/com/example/linebot/replier/BookOn.java
@@ -1,2 +1,19 @@
-package com.example.linebot.replier;public class book {
+package com.example.linebot.replier;
+
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
+
+public class BookOn implements Replier{
+
+    private final  String text;
+
+    public BookOn(String text){
+        this.text = text;
+    }
+
+    @Override
+    public Message reply(){
+        return new TextMessage(text);
+    }
+
 }
