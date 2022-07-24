c265de26b2c12ef0c2d42ace63528a8e68aa54a3
lesson1 <b2201980@photon.chitose.ac.jp>
2022-07-25 06:29:22 +0900

    返信用のインターフェイスを作成した

--- linebot-main/src/main/java/com/example/linebot/replier/Replier.java
+++ linebot-main/src/main/java/com/example/linebot/replier/Replier.java
@@ -3,7 +3,7 @@ package com.example.linebot.replier;
 import com.linecorp.bot.model.message.Message;
 
 // 返信用クラスのためのインターフェース
-public interface Reply {
+public interface Replier {
 
     // 返信用クラスが必ず実装するメソッド
     Message reply();
