3b1a90f40dc267d7090b30c1c9d61d747b63bd48
lesson1 <b2201980@photon.chitose.ac.jp>
2022-07-25 10:36:02 +0900

    EC2に対応した

--- book-API/main.py
+++ book-API/main.py
@@ -12,27 +12,32 @@ model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt')
 @app.route("/", methods=["POST"])
 
 def get():
-	# 写真読み込み
-	img = Image.open(BytesIO(request.data))
-	
+    # 写真読み込み
+    img = Image.open(BytesIO(request.data))
 
-	# 推論
-	results = model(img)
-	d = []
-	dic = {}
-	for value in results.pandas().xyxy[0].name:
-  		dic['result'] = value
-  		d.append(dic)
-	message = '本はありません'
-	if len(d) != 0:
-		message = ''
-		for i in d:
-			message  = message + i.get("result") + '\n'
 
-		message = message.rstrip('/r/n')
+    # 推論
+    results = model(img)
+    d = []
+    dic = {}
+    num = 0
+    for value in results.pandas().xyxy[0].name:
+        key = 'result' + str(num)
+        dic[key] = value
+        d.append(dic)
+        num += 1
+    message = '本はありません'
+    num = 0
+    if len(d) != 0:
+        message = ''
+        for i in d:
+            message  = message + i.get("result{}".format(str(num))) + '\n'
+            num += 1
 
-	return message
-  		
+        message = message.rstrip('/r/n')
+
+    return message
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
